27b2317571ace54131c5c824781d63c615658475
Jesper Jacobsen <jj@imusic.dk>
2021-01-26 22:02:20 +0100

    Fixed initial issues

--- src/Models/Credential.php
+++ src/Models/Credential.php
@@ -7,5 +7,15 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Credential extends Model
 {
-    use HasFactory;
+	use HasFactory;
+
+	/**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+		'credId',
+		'key',
+	];
 }
